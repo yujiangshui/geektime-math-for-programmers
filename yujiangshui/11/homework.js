6b8d6bfe260844b8477d01468da1b5614f4fc49a
harryyu <hi@harryyu.me>
2019-11-14 23:35:53 +1300

    feat: add search function

--- yujiangshui/11/homework.js
+++ yujiangshui/11/homework.js
@@ -29,4 +29,37 @@ function buildTree(dict) {
 }
 
 const dictTree = buildTree(dictionary);
-console.log('dictTree: ', JSON.stringify(dictTree));
+// console.log('dictTree: ', JSON.stringify(dictTree));
+
+function searchTree(tree, word) {
+  let wordChars = word;
+  if (typeof word === 'string') {
+    wordChars = word.split('');
+  }
+  const firstChar = wordChars.shift();
+  const existItem = tree.find(item => item.char === firstChar);
+  // 当前这一层没找到这个字符，终止
+  if (!existItem) {
+    return false;
+  }
+  // 当前是叶子结点，但是 word 还有字符，终止
+  if (existItem.children.length === 0 && wordChars.length > 0) {
+    return false;
+  }
+  // 当前 word 没了，叶子结点还有，查看是否是一个单词，例如词典：abc, abcd search abc
+  // ？？如何判断是否是一个合法单词？?
+  if (wordChars.length === 0) {
+    // todo if word is a valid word, then return true else false
+  }
+  if (wordChars.length === 0 && existItem.children.length === 0) {
+    return true;
+  }
+  return searchTree(existItem.children, wordChars);
+}
+
+console.log(searchTree(dictTree, 'abc'));
+console.log(searchTree(dictTree, 'adsafwc'));
+console.log(searchTree(dictTree, 'geek'));
+console.log(searchTree(dictTree, 'geometry'));
+console.log(searchTree(dictTree, 'geometr'));
+console.log(searchTree(dictTree, 'geometryy'));
